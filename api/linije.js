eec3de248be162ac54d1bfef2a33c6e8af22258e
l76000 <colakovicl035@gmail.com>
2025-11-20 17:50:00 +0100

    Change title from 'GSP Live (Anti-Cache)' to 'Linije'

--- api/linije.js
+++ api/linije.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>GSP Live (Anti-Cache)</title>
+    <title>Linije</title>
  
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
  
