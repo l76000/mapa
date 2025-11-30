f6ac4c29a53e164b763c972ab62f3a72803df07d
l76000 <colakovicl035@gmail.com>
2025-11-30 05:31:44 +0100

    Change import to require for googleapis

--- api/update-departures-sheet.js
+++ api/update-departures-sheet.js
@@ -1,6 +1,6 @@
-import { google } from 'googleapis';
+const { google } = require('googleapis');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -599,4 +599,4 @@ export default async function handler(req, res) {
               },
               cell: {
                 userEnteredFormat: {
-                  backgroundColor: { red: 0
+                  backgroundColor: {
